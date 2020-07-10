4d409cfdf433436264f3380504c679623c8e2885
Christopher MacGown <chris@macgown.com>
2020-07-10 15:40:12 -0700

    Bump pyarrow to support installing in python 3.8.3.

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     author_email='andrew.w.gross@gmail.com',
     url='https://github.com/andrewgross/json2parquet',
     install_requires=[
-        'pyarrow==0.14.0',
+        'pyarrow==0.17.0',
         'pandas==0.24.2',
         'numpy>=1.14.0',
         'ciso8601'
